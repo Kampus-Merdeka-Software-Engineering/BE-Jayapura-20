65ea7532d56341e2d9cf48055a0c3d15632e3b32
sintanastalia <sintanastalia25@gmail.com>
2023-10-07 22:41:34 +0800

    update link

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 
 app.use(
     cors({
-        origin: ["fe-jayapura-20-production.up.railway.app"],
+        origin: ["https://fe-jayapura-20-production.up.railway.app"],
     })
 )
 
